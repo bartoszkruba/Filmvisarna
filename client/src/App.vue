6a1e89a96e841118cad4eeec130c452ba45f377e
Rickard Andersson <rickard.andersson98@gmail.com>
2019-01-11 16:19:50 +0100

    Made a navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,24 +1,57 @@
 <template>
+  <header>
+    <b-navbar toggleable="md" type="dark" variant="primary">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
+      <b-navbar-brand>
+        <router-link class="router-link" to="/">Filmvisarna</router-link>
+      </b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="router-link" to="/">Hem</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link class="router-link" to="/moviesPage">Filmer</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link class="router-link" to="/">Kontakta Oss</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Sök Film"/>
+            <b-button size="sm" class="my-2 my-sm-0 mr-3" type="submit">Sök</b-button>
+          </b-nav-form>
+
+          <b-navbar-nav right>
+            <b-nav-item>
+              <router-link class="router-link" to="/">Logga In</router-link>
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
 </template>
 
 <script>
-
-
 export default {
   name: "Header",
   data() {
-    return {
-      
-    };
+    return {};
   },
-  methods: {
-    
-  }
+  methods: {}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.router-link {
+  text-decoration: none;
+  color: black;
+}
 </style>
