eb9e7cad4f069ced7f318db1d30d9213875fd89b
Rickard Andersson <rickard.andersson98@gmail.com>
2019-01-11 15:24:13 +0100

    Added branch Header and created a new Header vy

---
+++ client/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+
+<script>
+
+
+export default {
+  name: "Header",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
