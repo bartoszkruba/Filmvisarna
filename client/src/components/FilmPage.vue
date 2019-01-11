d32f1cb8ddd636705f242230fb77bbc713ca1702
Anna <zoora1234@hotmail.com>
2019-01-11 15:36:07 +0100

    Made a filmPage vy

---
+++ client/src/components/FilmPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>Filmer</h1>
+  </div>
+</template>
+
+<script>
+import api from "@/services/Api.js";
+
+export default {
+  name: "FilmPage",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      movies: null
+    };
+  },
+  methods: {
+    async getMovies() {
+      const response = await api.getMovies();
+      this.movies = response.data.movies;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
