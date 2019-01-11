ff2a1c1367ad72b924b7dfbd7e20e6cc0619c930
Rami <rami.albadri77@gmail.com>
2019-01-11 15:55:50 +0100

    KontaktSida skapat

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div v-for="m in movies">
       <h2>{{m.Title}}</h2>
       <p>{{m.Description}}</p>
     </div>
-    <button @click="getMovies" class="btn btn-warning" >Get Movies</button>
-  </div>
+  
 </template>
 
 <script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import KontaktSida from '@/components/KontaktSida'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/kontaktSida',
+      name: 'KontaktSida',
+      component: KontaktSida
     }
+
   ]
 })
