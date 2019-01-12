496d8a0fe10a43d067d29f66f83d37de55cd55c3
Bartosz Kruba <bartosz.kruba@gmail.com>
2019-01-12 12:28:15 +0100

    Adminpage Vy

---
+++ client/src/components/AdminPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Admin Page</h1>
+  </div>
+</template>
+
+<script>
+import api from "@/services/Api.js";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      movies: null
+    };
+  },
+  methods: {
+    async getMovies() {
+      const response = await api.getMovies();
+      this.movies = response.data.movies;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
