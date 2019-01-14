efc77474f4c8a76479b0670ea3a3758f386e4a2e
Anna <zoora1234@hotmail.com>
2019-01-14 14:49:49 +0100

    måsvinge

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -10,7 +10,6 @@ export default{
 		return api.post('addMovie', movie);
 	},
 
-	}, 
 	// Gets movies with matching query from DB
 	// Example: getMovies({Title: 'Batman'}) - gets all movies with title "Batman"
 	getMovies: (query) => {
