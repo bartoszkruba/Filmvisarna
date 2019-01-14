e63b6f164c7f5c0b9fbb906d8b30b432476dc66c
Rami <rami.albadri77@gmail.com>
2019-01-14 21:53:13 +0100

    fixat bokningsida

--- client/src/components/BokningSida.vue
+++ client/src/components/BokningSida.vue
@@ -1,38 +1,64 @@
 <template> 
 <div>
-    <img src ="https://i.ytimg.com/vi/xqj7XOv9mC8/maxresdefault.jpg" class="papillon float-right">
-   <p>
-       {{movies}}
-      <h4><label class=" ">Antal bilijett(er):</label></h4>
-      <select id="antal-bilijetter">
-        <option value="one" class="one">1</option>
-        <option value="two" class="">2</option>
-        <option value="three" class="">3</option>
-        <option value="thour" class="">4</option>
-        <option value="five" class="">5</option>
-        <option value="sex" class="">6</option>
-        <option value="sven" class="">7</option>
-      </select>
-      <button @click="getMovies">getMovies</button>
-    <p>bablablalba</p>
+    <div>
+        <div class="papillon"></div>
+    <img src ="https://i.ytimg.com/vi/xqj7XOv9mC8/maxresdefault.jpg">
+  </div> <p>
+    <!-- {{movies}} -->
+
+    <div class="text"> 
+        <div class="antal-bilijetter">
+           <h4>Antal bilijetter:</h4>
+        </div>
+        <div class="antal">
+           <button v-on:click="minus" type="button" class="btn btn-dark">+</button>
+           <h5 class="hej"> {{antal}}st/{{pris}}kr </h5>
+           <button v-on:click="plus" type="button" class="btn btn-dark">+</button>
+        </div>
+        <div>
+           <button type="button" class="slutför btn btn-danger">Slutför bokning</button>
+        </div>
+    </div>
+
 </div>
 
 </template> 
  
 <script> 
+let pris = 150;
+let antal=1;
 import api from "@/services/Api.js"; 
+
  
 export default {
   name: "BokningSida", 
   data() { 
     return { 
+        antal: null,
+        pris: null,
         movies: null
     }; 
   }, 
+   created(){
+      this.getMovies();
+      this.plus();
+        },
   methods: {
       async getMovies(){
           this.movies = await api.getMovies();
           this.movies = this.movie.data
+      },
+      plus(){
+          this.pris+= 150;
+          this.antal+=1;
+      },
+      minus(){
+          if (this.pris>150){
+          this.pris-= 150;
+          this.antal-=1;}
+          else {
+              alert('Du kan inte välja mindre än ett biljett ')
+          }
       }
   } 
 }; 
@@ -40,15 +66,58 @@ export default {
  
 <!-- Add "scoped" attribute to limit CSS to this component only --> 
 <style > 
-h4{
-    width: 20vw;
-    margin-right: 0;
+main{
+    background-color: black;
 }
+.antal-bilijetter{
+    display: flex;
+    justify-content: center;
+    margin: 3vh 0;
+}
+.bokning{
+    margin: 3vh;
+}
+h4, h5{
+    color: white;
+}
+
 .papillon{
-    margin: 3vh 1vw 3vh 0;
-   float: right;
-   height: 25vh;
-   width: 25vw;
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(180deg,rgba(14,15,15,0) 50%,#0e0f0f);
+    height: 37.3vh;
 }
+img{
+    height: 30vh;
+    width: 100%;
+}
+
+.antal{
+    margin: 0;
+    display: flex;
+    justify-content:space-around;
+}
+.slutför{
+    margin: 5vh auto
+}
+.text{
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+}
+.hej{
+    margin: 0 2vw;
+}
+@media screen and (min-height: 600px) {
+    .papillon{
+    height: 38vh;
+}
+    
+}
+
 
 </style> 
