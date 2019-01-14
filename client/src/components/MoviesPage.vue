d74036fc31c36eb900696c98cb8b7c5005951edf
Anna <zoora1234@hotmail.com>
2019-01-14 12:37:05 +0100

    some picture and minor things

--- client/src/components/MoviesPage.vue
+++ client/src/components/MoviesPage.vue
@@ -1,6 +1,24 @@
 <template>
+
+
+
   <div class="hello">
-    <h1>Filmer</h1>
+    <h1>{{ msg }}</h1>
+    <div v-for="m in movies">
+      <h2>{{m.Title}}</h2>
+      <p>{{m.ProductionCountries}}</p>
+      <p>{{m.ProductionYear}}</p>
+      <p>{{m.Length}}</p>
+      <p>{{m.Genre}}</p>
+      <p>{{m.Language}}</p>
+      <p>{{m.Subtitle}}</p>
+      <p>{{m.Director}}</p>
+      <p v-for="actor in m.Actors">{{actor}}</p>
+      <p>{{m.Description}}</p>
+      <p>{{m.Age}}</p>
+      <p> <img src="../assets/Insidious.jpg" alt=""></p>
+    </div>
+    <button @click="getMovies" class="btn btn-warning" >Get Movies</button>
   </div>
 </template>
 
@@ -27,3 +45,4 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
+
