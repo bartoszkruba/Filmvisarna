efc0a3cd9d22fd9741fd7b5496d441bff72bd1cf
Anna <zoora1234@hotmail.com>
2019-01-14 11:28:36 +0100

    Updated databas and how to show it on screen

--- backend/src/models/movie.js
+++ backend/src/models/movie.js
@@ -8,7 +8,37 @@ const movieSchema = schema({
     Title: {
         type: String
     },
+    ProductionCountries: {
+        type: String
+    },
+    ProductionYear: {
+        type: String
+    },
+    Length: {
+        type: String
+    },
+    Genre: {
+        type: String
+    },
+    Language: {
+        type: String
+    },
+    Subtitle: {
+        type: String
+    },
+    Director: {
+        type: String
+    },
+    Actors: {
+        type: Array
+    },
     Description: {
+        type: String
+    },
+    Age: {
+        type: String
+    },
+    Image: {
         type: String
     }
 });
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -3,7 +3,18 @@
     <h1>{{ msg }}</h1>
     <div v-for="m in movies">
       <h2>{{m.Title}}</h2>
+      <p>{{m.ProductionCountries}}</p>
+      <p>{{m.ProductionYear}}</p>
+      <p>{{m.Length}}</p>
+      <p>{{m.Genre}}</p>
+      <p>{{m.Language}}</p>
+      <p>{{m.Subtitle}}</p>
+      <p>{{m.Director}}</p>
+
+      <p v-for="actor in m.Actors">{{actor}}</p>
       <p>{{m.Description}}</p>
+      <p>{{m.Age}}</p>
+      <p> <img src="../assets/Insidious.jpg" alt=""></p>
     </div>
     <button @click="getMovies" class="btn btn-warning" >Get Movies</button>
   </div>
