37a1d80dd0e8188b37f8f90125e4d3f717e57b50
Kristoffer <doedmannen@aetmej.se>
2019-01-15 09:11:11 +0100

    Added search functionality

--- client/src/components/Movie.vue
+++ client/src/components/Movie.vue
@@ -1,3 +1,4 @@
+<<<<<<< current
 <!-- INTE KOPPLAD MOT DATABAS -->
 
 <template>
@@ -157,3 +158,138 @@ li{
 }
 
 </style>
+=======
+<!-- INTE KOPPLAD MOT DATABAS -->
+
+<template>
+  <div class="Movie">
+
+    <section v-if="aMovie === null">
+      <p>Något blev galet!</p>
+      <p>Det finns ingen film med det ID du angav </p>
+      <p>Länk tillbaka till MoviesPage</p>
+    </section>
+
+    <b-jumbotron bg-variant="white">
+
+      <!-- Title -->
+      <template slot="header">
+        {{aMovie.title}}
+      </template>
+      <!-- description -->
+      <template slot="lead">
+          <span v-html="aMovie.description"></span>
+      </template>
+      <b-btn variant="primary" href="#">Boka platser</b-btn>
+
+
+      <hr class="my-4">
+      <section class="trailer-view">
+        <section class="trailer-video">
+          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Z9AYPxH5NTM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </section>
+        <section class="trailer-text">
+          <ul>
+            <li>Genre: {{aMovie.tenre}}</li>
+            <li>Längd: {{aMovie.length}}</li>
+            <li>År: {{aMovie.productionYear}}</li>
+            <li>Land:<span v-for="c in aMovie.ProductionCountries"> {{c}}</span></li>
+            <li>Språk: {{aMovie.language}}</li>
+            <li>Undertexter: {{aMovie.subtitles}}</li>
+            <li>Regisör: {{aMovie.director}}</li>
+          </ul>
+          <ul>
+            <li>Skådespelare:</li>
+            <li v-for="actor in aMovie.actors">{{actor}}</li>
+          </ul>
+        </section>
+        <section class="trailer-text">
+          <p>
+            Quote
+          </p>
+
+        </section>
+      </section>
+      <hr class="my-4">
+
+
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+import api from "@/services/Api.js";
+
+export default {
+  name: "Movie",
+  data() {
+    return {
+      aMovie: null,
+      movieID: null
+    };
+  },
+  computed: {
+    movieID: function(){
+      if((window.location.hash.indexOf("?")+1) > 0)
+        return window.location.hash.substr(window.location.hash.indexOf("?")+1);
+      return null;
+    }
+  },
+  methods: {
+    async getMovieByID(id = this.movieID) {
+      const response = await api.getMovieByID(id);
+      this.aMovie = response.data.movie;
+    },
+
+  },
+  created: function() {
+    this.getMovieByID();
+  },
+  mounted: function() {
+    this.getMovieByID();
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+  box-sizing: border-box;
+}
+.trailer-view{
+  display: flex;
+  flex-direction: column;
+}
+.trailer-text{
+  flex: 1;
+  order: 2;
+}
+.trailer-video{
+  display: flex;
+  justify-content: space-around;
+  flex: 1;
+}
+
+.combined-view{
+  display: flex;
+}
+.combined-view > section{
+  flex: 1;
+}
+
+li{
+  list-style: none;
+}
+
+.movieposter{
+  width: 100%;
+}
+
+@media screen and (min-width: 1024px) {
+  .trailer-view{
+    flex-direction: row;
+  }
+}
+
+</style>
+>>>>>>> before discard
