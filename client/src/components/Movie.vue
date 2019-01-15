c17261860e6201f268ffebbdcbdc6c26ec6e6486
Kristoffer <doedmannen@aetmej.se>
2019-01-15 16:58:40 +0100

    responsivitet till video trailer

--- client/src/components/Movie.vue
+++ client/src/components/Movie.vue
@@ -3,21 +3,29 @@
 <template>
   <div class="Movie">
 
-    <section v-if="aMovie === null">
-      <p>Något blev galet!</p>
-      <router-link class="router-link" to="/moviesPage" exact-active-class="menu-item-active">Klicka här för att komma till alla filmer</router-link>
+    <b-jumbotron bg-variant="white">
+      <section v-if="aMovie === null">
+        <h1>Något blev fel!</h1>
+        <p>Vi hittade ingen film med det ID som angavs. Det kan bero på något av följande</p>
+        <ul>
+          <li>Antipiratbyrån har hackat oss</li>
+          <li>Vår hemsida har tekniskt strul</li>
+          <li>Du har klickat på en gammal länk</li>
+        </ul>
+        <router-link class="router-link" to="/moviesPage" exact-active-class="menu-item-active">Klicka här för att komma till alla filmer</router-link>
 
-    </section>
+      </section>
 
-    <section v-else>
+      <section v-else>
 
-      <b-jumbotron bg-variant="white">
 
         <!-- Title -->
         <section class="movieheader">
+
           <section class="movieheader_pic">
             <img class="movieposter" :src="require('../assets/'+this.aMovie.images[0])">
           </section>
+
           <section class="movieheader_text">
             <h1 class="movietitle">
               {{aMovie.title}}
@@ -29,13 +37,13 @@
           </section>
         </section>
 
-        <b-btn variant="primary" href="#">Boka platser</b-btn>
+        <b-btn variant="danger" href="#">Boka platser</b-btn>
 
 
         <hr class="my-4">
         <section class="trailer-view">
           <section class="trailer-video">
-            <iframe width="100%" height="100%" :src="'https://www.youtube.com/embed/'+this.aMovie.youtubeTrailers[0]" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe class="videoplayer" :src="'https://www.youtube.com/embed/'+this.aMovie.youtubeTrailers[0]" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </section>
           <section class="trailer-text">
             <ul>
@@ -47,15 +55,21 @@
               <li>Undertexter: {{aMovie.subtitles}}</li>
               <li>Regisör: {{aMovie.director}}</li>
             </ul>
-            <ul>
+            <ul v-if="aMovie.actors.length > 0">
               <li>Skådespelare:</li>
               <li v-for="actor in aMovie.actors">{{actor}}</li>
             </ul>
           </section>
           <section class="trailer-text">
-            <p>
-              Quote
-            </p>
+            <article v-for="review in aMovie.reviews">
+              <p>
+                <span class="reviewHead">{{review.source}}</span>
+                <span class="star activeStar">{{starView(0, (review.stars/1))}}</span><span class="star inactiveStar">{{starView((review.stars/1), (review.max/1))}}</span>
+                <br>
+                <em>{{review.quote}}</em>
+              </p>
+
+            </article>
 
 
           </section>
@@ -64,8 +78,8 @@
 
 
 
-      </b-jumbotron>
-    </section>
+      </section>
+    </b-jumbotron>
   </div>
 </template>
 
@@ -82,8 +96,13 @@ export default {
   methods: {
     getImageUrl(){
       return { sample: require('../assets/'+this.aMovie.images[0]) };
-    }
-    ,
+    },
+    starView(s, n){
+      let starPut = "";
+      for(let i = s; i < n; i++)
+        starPut += "*";
+      return starPut;
+    },
     async getMovieByID() {
       if(this.movieID() !== null){
         try{
@@ -120,6 +139,7 @@ export default {
 *{
   box-sizing: border-box;
 }
+
 .trailer-view{
   display: flex;
   flex-direction: column;
@@ -128,10 +148,17 @@ export default {
   flex: 1;
   order: 2;
 }
+
 .trailer-video{
   display: flex;
   justify-content: space-around;
   flex: 1;
+  margin-bottom: 35%;
+}
+
+.videoplayer{
+  height: 200%;
+  width: 100%;
 }
 
 .combined-view{
@@ -149,11 +176,12 @@ export default {
 
 .movieheader_pic{
   flex: 1;
+  text-align: center;
 }
 
 .movieheader_text{
   flex: 5;
-  padding: 0 2%;
+  padding: 3% 2%;
 }
 
 .moviedescription{
@@ -173,16 +201,45 @@ li{
 }
 
 .movieposter{
-  width: 100%;
+  width: auto;
+  height: 50vh;
 }
 
+.star{
+  margin: 0;
+  padding: 0;
+  font-weight: bold;
+  font-size: 180%;
+  text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
+}
 
-@media screen and (min-width: 1024px) {
+.reviewHead{
+  font-size: 200%;
+}
+
+.activeStar{
+  color: rgb(233, 255, 0);
+}
+.inactiveStar{
+  color: rgb(122, 122, 122);
+}
+
+@media screen and (min-width: 768px) {
   .trailer-view{
     flex-direction: row;
   }
   .movieheader{
     flex-direction: row;
+  }
+  .movieposter{
+    width: 100%;
+  }
+  .trailer-video{
+    margin-bottom: 0;
+  }
+
+  .videoplayer{
+    height: 100%;
   }
 }
 
