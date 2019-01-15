1fbe97fdbe56dd89e310af98e848a3a1819cf1f8
Unknown <filip98@spray.se>
2019-01-15 15:33:58 +0100

    start page

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -10,7 +10,7 @@ export default {
 		return api.post('addMovie', movie);
 	},
 	// Gets movies with matching query from DB
-	// Example: getMovies({Title: 'Batman'}) - gets all movies with title "Batman"
+	// Example: getMovies({title: 'Batman'}) - gets all movies with title "Batman"
 	getMovies: (query) => {
 		return api.post('movies', { query: query });
 	},
