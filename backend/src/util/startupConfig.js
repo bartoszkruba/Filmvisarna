ddcb8c4bf6f161de4b351097b4dc1f6246459cce
Bartosz Kruba <bartosz.kruba@gmail.com>
2019-01-16 21:43:45 +0100

    l33t

---
+++ backend/src/util/startupConfig.js
@@ -0,0 +1,52 @@
+const port = 8081;
+
+module.exports.port = port;
+
+module.exports.startupMessage = `
+                              .,codxO00KXKKXNXK0kdl;..                          
+                               .',,,;;;;;;,,;;,;;;'.                            
+  .cdc;lddood,   .cxxxo,..;dxxdc..cddc.,oddd;.cdddo:.'od;.cdl. .ldddc.'oxkxl'   
+  ,KM0ccOWMKd,  .xWXdOMK;:XWOkWWl,0MMXlxMMMMo,OMXkKWolNMd,OMK, ,KMXx:;OMKxKNx.  
+  '0M0' cNMx.   'OM0;lX0coWWdlNMd;OWWN0KMWWMo,OMKoOMxoNMd,OMK, ,KMKl''kWXko:.   
+  ,0M0' lWMx.   'OM0,.;,.oWWdlNMd;OW0KWNX0XMo,0MN0Ok;cNMd,OMK, ;KMN0o..lONN0:.  
+  ,0M0' lWMx.   .OM0;lK0:oWWolNMd;0WkOWXOx0Mo,0MK:.. cNMd,OMK; ,KM0, .lkoc0W0,  
+  ,0M0' lWMx.   .oNXk0WO,;KW00WNc,0MdoNOll0Mo,0M0,...cNMd,OMWKolKMN0x:dWKkKWk.  
+  .;c;. .:c,     .;lolc'..,cllc:''co;'c;.';c'.;c;....':c'.;lcl;';ccc:..;cll:.   
+                  ........''''..,',,'................         ...               
+                  .....'..,,;,......''.....',,,,,',''.     .,;:;..              
+               ..;c;,'','.,:ll,....',,,;:;;::;;;;;;;;,..  .'xOkdl;'.            
+             ..;ldOko,....;clo:'...,;cllll:,',,,,,,,,,'.  ..xXK0kdl:'.          
+           .';ldkO0K0o....',,;;::;;,;lddlc,''..,;,'.'''....,xXXXK0Odl:'.        
+         .':ldxOO0Okxd:..'.';:c:,,,,';ll:;'',,,;:;,,,,,,...;dO00KKK0Oxl:'.      
+        .;coxkkkxdlc:,.....,:cc::::::cddlc,',;::::;;,,,,'.',',coxkOOOOkxo:..    
+       .':ldxxol:,..     .;clclllcclloxdoc;,:cccc::,,,,,,'''.  .';codxkxdl;.    
+        .';::;'..        .:loodxxollodxdol;,;::ccc:;,,,,;;;'      ..';ccc:,.    
+         ...             .:ooodxdoccoolc;,'',;;::::;,,,,,;;'          .....     
+                          ,odoll:,;lxxdc:::;;::::;;,,,,,....                    
+                          .:doc:;,:lllllllc:;;,,;;;,,,,,.                       
+                           .colccllclllllc::;;,;;;;,,,,'.                       
+                            .':llllloolc::::;;;;;,,,,,,..                       
+                              .:llcclooooolc:;;;;,,''','                        
+                               .:cc:codollc;,,;;,'.',,;.                        
+                                .;c;;:c:;,'.......',,;,.                   .    
+                    ..........   .cc::;;,'......'',,;,,'.                .','.  
+    ......        .';::::::;;,.. .ldlc::::,'..',,,,;,,''.               .;ccc:..
+ ..,;::::;,..   .':cooooooodo:.. .:xxoc::::;''',;;,,,'.,.   ..',,,,'....,ldxxo:;
+..,:clllool:,. .':loolc::col,. .. .lkkxdolc:,',,,,,''',:'   .,:clllc:,'';oxkxdlc
+ ..,;;:clool:...;lool,. ....       .lkOkkkxoc;'''''';:::.    .,:cloooc;',cdxdd:;
+ ..,;;;:lddoc'..,;;,..              'dkkkkkkkxo;';:c:::;.      ..,:lol;..,cooc'.
+.';clllllol:'.....                .  'okkkOkOOd'.':ccc:'            ......;cl:..
+':c:;,,'........ .,;;;'. .,,..','..','.:dddddo,.  .,cc:'.,,''''.          ......
+,;..   ........ ,kNXKNXo.lNWd:OWX:;KN0,:XXNXKK0c.  ,0N0oxXNNNXKc               
+..         .....lNMkcdOo.oWMkc0MN::NMK,cNWMOcOMk.  ;KMK:.:0MWx'.               
+           .    ,OWN0d:. oWMNKNMN::NMK,cNWMKxKMk.  ;XMX; .OMWl                 
+           ..   .'lxKWNx'oWMKONMN::NMK,cNWMNOkd,   ;XMX; .OMWl           ..... .
+                ;O0l:0MXcoWMx:0MN::NMK,cNWMO.      ;XMX; .OMWl          ...... .
+                ,0WKkXW0,oWMx:0MN::NMK,cNWMO.  .   ;XMX; .OMWl  ....   ....... .
+           ..... .:lol:. 'cc'.;c:..:c;..:cc;.      .;c;. .,c:'.  ..   .......  .
+             ...                                ...           ....   ......    .
+
+Listening on ${port}
+`
+
+
