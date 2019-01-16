7104765b9df93a78a7ab585608d79672c5cb83cf
Bartosz Kruba <bartosz.kruba@gmail.com>
2019-01-16 11:47:01 +0100

    Api update for adding movies

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -8,8 +8,8 @@ const api = axios.create({
 
 export default {
 
-	addMovie(movie) {
-		return api.post('addMovie', movie);
+	addMovie(movie, user) {
+		return api.post('addMovie', {movie, user});
 	},
 	// Gets movies with matching query from DB
 	// Example: getMovies({title: 'Batman'}) - gets all movies with title "Batman"
