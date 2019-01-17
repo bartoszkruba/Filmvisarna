4dd4868a051197fefd165c742fd9b953d06b4830
Bartosz Kruba <bartosz.kruba@gmail.com>
2019-01-17 11:11:17 +0100

    Backend returns movie sessions sorted - based on date

--- backend/src/controllers/movieSessionController.js
+++ backend/src/controllers/movieSessionController.js
@@ -4,6 +4,17 @@ const MovieTheatre = require('../models/movieTheatre');
 module.exports.postMovieSessions = async (req, res, next) => {
     try {
         const movieSessions = await MovieSession.find(req.body.query);
+        movieSessions.sort((a, b) => {
+            const firstSessionDate = a.date.year.toString()
+                + a.date.month.toString().padStart(2, '0')
+                + a.date.day.toString().padStart(2, '0')
+                + a.date.time.replace(':', '');
+            const secondSessionDate = b.date.year.toString()
+                + b.date.month.toString().padStart(2, '0')
+                + b.date.day.toString().padStart(2, '0')
+                + b.date.time.replace(':', '');
+            return firstSessionDate > secondSessionDate ? 1 : -1;
+        })
         res.send({
             movie_sessions: movieSessions
         })
@@ -15,7 +26,7 @@ module.exports.postMovieSessions = async (req, res, next) => {
     }
 }
 module.exports.postAddMovieSession = async (req, res, next) => {
-    const movieTheatre = await MovieTheatre.find({_id: req.body.movieSession.movieTheatreID});
+    const movieTheatre = await MovieTheatre.find({ _id: req.body.movieSession.movieTheatreID });
     new MovieSession({
         movieID: req.body.movieSession.movieID,
         date: req.body.movieSession.date,
