81a7cedca5278c24e508e864d4cf13c76968c5da
Rickard Andersson <rickard.andersson98@gmail.com>
2019-01-17 13:45:05 +0100

    sprint 1 demo

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -9,7 +9,6 @@
         <b-navbar-nav>
           <b-nav-item class="router-link" to="/" exact-active-class="menu-item-active" exact>Hem</b-nav-item>
           <b-nav-item class="router-link" to="/moviesPage" exact-active-class="menu-item-active">Filmer</b-nav-item>
-           <b-nav-item class="router-link" to="/kalendarium" exact-active-class="menu-item-active">Kalendarium</b-nav-item>
           <b-nav-item class="router-link" to="/kontaktSida" exact-active-class="menu-item-active">Kontakta Oss </b-nav-item>
         </b-navbar-nav>
 
