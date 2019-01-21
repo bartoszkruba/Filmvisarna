40978a8c03d739daea1eaa12b905a9ec92c833ed
Kristoffer <doedmannen@aetmej.se>
2019-01-21 16:04:31 +0100

    Changed opening hours

--- client/src/components/KontaktSida.vue
+++ client/src/components/KontaktSida.vue
@@ -18,7 +18,7 @@
 
     <div class="kontakta">
       <h3>Öppettider</h3>
-      <p>Öppet måndag - fredag 11:00-21:00, lördag - söndag 10:00-21:00</p>
+      <p>Öppet måndag - fredag 11:00-23:45, lördag - söndag 10:00-23:45</p>
       <p>Julafton stängt</p>
       <p>Nyårsafton 10:00-15:00</p>
       <p>Nyårsdagen 11:00-17:00</p>
@@ -55,8 +55,8 @@
       <p>Storagatan, 213 56 Malmö</p>
     </div>
   </div>
-</template> 
- 
+</template>
+
 <script>
 import api from "@/services/Api.js";
 
@@ -67,10 +67,10 @@ export default {
   },
   methods: {}
 };
-</script> 
- 
-<!-- Add "scoped" attribute to limit CSS to this component only --> 
-<style scoped> 
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 h1{
   margin-top: 3vh;
   text-align: center;
@@ -88,4 +88,4 @@ hr {
   border-color: rgb(185, 184, 184);
   margin-top: 3rem;
 }
-</style> 
+</style>
