b01415751c0e85979be8bd17dc7c0b6b5b6cd5d0
Rickard Andersson <rickard.andersson98@gmail.com>
2019-01-23 23:13:51 +0100

    safety push

--- client/src/components/MovieTheatres/MovieSaloon.vue
+++ client/src/components/MovieTheatres/MovieSaloon.vue
@@ -11,7 +11,7 @@
     <section class="screen">Bioduk</section>
     <section class="seats mt-5">
       <template v-for="(rows,index) in seatsPerRow">
-        <section class="rows" v-bind:class="index">
+        <section class="rows" v-bind:id="rows">
           <template v-for="(seat, index2) in seatsPerRow[index]">
             <MovieSeat
                @hoverOverSeat="hoverOverSeat"
@@ -47,8 +47,7 @@ export default {
       freePlaces: null,
       choosenSeats: [],
       leavingSeat: false,
-      seatsToHover: []
-      
+      seatsToHover: [],
     };
   },
   components: {
@@ -92,25 +91,20 @@ export default {
     hoverOverSeat(e,hover){
       if(hover && this.mySeats > 0){
         this.seatsToHover = [];
-        this.seatsToHover.push(e.target.id);
-        console.log(e)
         if(this.mySeats > 1){
-            let index = this.freePlaces.findIndex(i => i.seatNumber === e.target.id);
+            if(e.target.id !== e.fromElement.lastElementChild.id) {
+              let index = this.freePlaces.findIndex(i => i.seatNumber === e.target.id);
             for(let i = index; i < this.mySeats + index; i++){
                 this.seatsToHover.push(this.freePlaces[i].seatNumber);
             }
+            }
+        }else{
+          this.seatsToHover.push(e.target.id);
         }
       }else{
         this.leavingSeat = !this.leavingSeat;
       }
     },
-
-    whatRow(rowID){
-      let thisRow = 0;
-      
-      return thisRow;
-    }
-   
   },
   computed: {
     moreSeats: function() {
