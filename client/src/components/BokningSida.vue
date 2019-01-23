e94a2b04b1b73d4a15d408398a5e5b28f30c0800
Rami <rami.albadri77@gmail.com>
2019-01-23 20:02:10 +0100

    bild på film + css på pop-uppen

--- client/src/components/BokningSida.vue
+++ client/src/components/BokningSida.vue
@@ -119,36 +119,43 @@
         </div>
         <!-- Modal Component -->
         <b-modal id="modal1" v-model="showTicketModal" title="Bekräftelse" @ok="goHem" ok-only no-close-on-esc no-close-on-backdrop hide-header-close>
-          <p>Film:
-            <strong>{{movie.title}}</strong>
-          </p>
-          <p>Datum:
-            <strong>{{this.session.date.day+'/'+this.session.date.month+' '+this.session.date.year }}</strong>
-          </p>
-          <p>Tid:
-            <strong>{{this.session.date.time}}</strong>
-          </p>
-          <p>Salong:
-            <strong>{{this.theatre.name}}</strong>
-          </p>
+        <div class="bekraftelse">
+          <div class="bekraftelseText"> 
+              <p>Film:
+                <strong>{{movie.title}}</strong>
+              </p>
+              <p>Datum:
+                <strong>{{this.session.date.day+'/'+this.session.date.month+' '+this.session.date.year }}</strong>
+              </p>
+              <p>Tid:
+                <strong>{{this.session.date.time}}</strong>
+              </p>
+              <p>Salong:
+                <strong>{{this.theatre.name}}</strong>
+              </p>
 
-          <div class="Biljetter">
-            <p>Biljetter:</p>
-            <div class="vilkaBiljetter">
-              <p v-if="antal>0">
-                <strong>{{antal}} Ordinarie</strong>
-              </p>
-              <p v-if="antalPensionar>0">
-                <strong>{{antalPensionar}} Pensionär</strong>
-              </p>
-              <p v-if="antalBarn>0">
-                <strong>{{antalBarn}} Barn</strong>
+              <div class="Biljetter">
+                <p>Biljetter:</p>
+                <div class="vilkaBiljetter">
+                  <p v-if="antal>0">
+                    <strong>{{antal}} Ordinarie</strong>
+                  </p>
+                  <p v-if="antalPensionar>0">
+                    <strong>{{antalPensionar}} Pensionär</strong>
+                  </p>
+                  <p v-if="antalBarn>0">
+                    <strong>{{antalBarn}} Barn</strong>
+                  </p>
+                </div>
+              </div>
+              <p>Att betala:
+                <strong>{{totalt}}kr</strong>
               </p>
             </div>
-          </div>
-          <p>Att betala:
-            <strong>{{totalt}}kr</strong>
-          </p>
+            <div class="bekraftelsePic">
+              <img :src="url + movie.imagesLinks.poster">
+            </div>
+        </div>
           <p class="my-4">Ditt bokningsnummer:
             <strong>{{bokningsnummer}}</strong>
           </p>
@@ -510,6 +517,18 @@ export default {
   }
 }
 
+.bekraftelse{
+  display: flex;
+}
+.bekraftelseText{
+  margin-top: 5vh;
+  width: 19vw;
+}
+.bekraftelsePic{
+  margin-top: 7vh;
+  width: 11vw; 
+}
+
 main{
   background-color: white;
 }
@@ -519,10 +538,7 @@ main{
 .mainBekraftelse{
   display: flex;
 }
-.img2{
-  width: 90%;
-  box-shadow: 2px 2px 20px black;
-}
+
 
 .modal-body p {
   margin: 0.8rem;
@@ -652,8 +668,36 @@ div .vilkaBiljetter {
 }
 
 
+@media screen and (min-width: 417px) and (max-width: 768px){
+  div .location {
+    width: 90vw;
+  }
+  .bekraftelseText{
+  margin-top: 5vh;
+  width: 35vw;
+}
+.bekraftelsePic{
+  margin-top: 7vh;
+  width: 21vw; 
+}
 
+
+}
 @media screen and (max-width: 416px) {
+  .bekraftelse{
+    display: flex;
+    flex-direction: column;
+  }
+  .bekraftelseText{
+    order:2;
+    margin-top: 3vh;
+    width: 86vw;
+  }
+  .bekraftelsePic{
+    display: flex;
+    margin-top: 1vh;
+    width: 65%; 
+    margin-left: 19% }
 
   title {
     margin-top: -10vh;
@@ -662,8 +706,8 @@ div .vilkaBiljetter {
     width: 90vw;
   }
   .mainBekraftelse{
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
   }
 
   .title {
